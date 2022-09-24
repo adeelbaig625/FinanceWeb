44131a57f05ffcfcf045124707b04bab50db6a12
adeelbaig625 <adeelbaig625@gmail.com>
2022-09-24 16:51:58 +0500

    theme changing

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,63 +1,80 @@
-import React from 'react'
-import './login.css'
-import { Signin } from '../../DB/FirestoreQueries';
-import {useNavigate} from 'react-router-dom'
-import { getAuth ,onAuthStateChanged} from 'firebase/auth'
+import React from "react";
+import "./login.css";
+import { Signin } from "../../DB/FirestoreQueries";
+import { useNavigate } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 function Login() {
-  const [email,setEmail]=React.useState("");
-  const navigate=useNavigate()
-  const [password,setPassword]=React.useState("");
-  const [loader,setLoader]=React.useState(false)
+  const [email, setEmail] = React.useState("");
+  const navigate = useNavigate();
+  const [password, setPassword] = React.useState("");
+  const [loader, setLoader] = React.useState(false);
 
-  const authenticate=(e)=>
-  {
+  const authenticate = (e) => {
     e.preventDefault();
-    setLoader(true)
-    Signin(email,password).then(res=>
-      {
-        navigate('/home', { replace: true })
-      }).catch(err=>
-        {
-          setLoader(false)
-        })
-  }
-  React.useEffect(()=>
-  {
-    const authref =  getAuth();
-    const unregisterAuthObserver =onAuthStateChanged(authref, async(user) => {
-      if(user)
-      {
-        navigate('/home', { replace: true })
+    setLoader(true);
+    Signin(email, password)
+      .then((res) => {
+        navigate("/home", { replace: true });
+      })
+      .catch((err) => {
+        setLoader(false);
+      });
+  };
+  React.useEffect(() => {
+    const authref = getAuth();
+    const unregisterAuthObserver = onAuthStateChanged(authref, async (user) => {
+      if (user) {
+        navigate("/home", { replace: true });
       }
-    
-            })
-    
-            return () => unregisterAuthObserver()
-  
-  })
+    });
+
+    return () => unregisterAuthObserver();
+  });
   return (
-    <div className='Login-body'>
-      <div className='Login-inner-container'>
-        <div className='right-container'>
-            <h1>Finance App</h1>
-            <h2>The <span style={{color:'#5AFF6A'}}>#1</span> personal finance & budgeting app.</h2>
-            <img src='/assets/loginVector.png'/>
+    <div className="Login-body">
+      <div className="Login-inner-container">
+        <div className="right-container">
+          <h1>Finance App</h1>
+          <h2>The #1 personal finance & budgeting app.</h2>
+          <img src="/assets/loginVector.png" />
         </div>
-        <div className='left-container'>
-          <div className='left-inner-container'>
+        <div className="left-container">
+          <div className="left-inner-container">
             <h1>Login</h1>
-            <form onSubmit={(e)=>authenticate(e)}>
-            <input placeholder='Email' type='email' required value={email} onChange={(e)=>setEmail(e.target.value)}/>
-            <input placeholder='Password' type='password' required value={password} onChange={(e)=>setPassword(e.target.value)}  autoComplete="new-password"/>
-            {!loader && <button type='submit'>Log in</button>}
-            {!loader &&<p>Don't have account? <span onClick={()=> navigate('/signup')} style={{textDecoration:'underline',cursor:'pointer'}}>Sign up</span></p>}
+            <form onSubmit={(e) => authenticate(e)}>
+              <input
+                placeholder="Email"
+                type="email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                placeholder="Password"
+                type="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                autoComplete="new-password"
+              />
+              {!loader && <button type="submit">Log in</button>}
+              {!loader && (
+                <p>
+                  Don't have account?{" "}
+                  <span
+                    onClick={() => navigate("/signup")}
+                    style={{ textDecoration: "underline", cursor: "pointer" }}
+                  >
+                    Sign up
+                  </span>
+                </p>
+              )}
             </form>
-           
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,51 +1,78 @@
-import React from 'react'
-import './signup.css'
-import { Signup as SignupQuery } from '../../DB/FirestoreQueries'
-import {useNavigate} from 'react-router-dom'
+import React from "react";
+import "./signup.css";
+import { Signup as SignupQuery } from "../../DB/FirestoreQueries";
+import { useNavigate } from "react-router-dom";
 function Signup() {
-  const navigate=useNavigate()
-  const [email,setEmail]=React.useState("");
-  const [name,setName]=React.useState("");
-  const [password,setPassword]=React.useState("");
-  const [loader,setLoader]=React.useState(false)
-  const authenticate=(e)=>
-  {
+  const navigate = useNavigate();
+  const [email, setEmail] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [loader, setLoader] = React.useState(false);
+  const authenticate = (e) => {
     e.preventDefault();
-    setLoader(true)
-    SignupQuery(name,email,password).then(res=>
-      {
-        navigate('/home')
-      
-      }).catch(err=>
-        {
-          console.log(err)
-          setLoader(false)
-        })
-      }
+    setLoader(true);
+    SignupQuery(name, email, password)
+      .then((res) => {
+        navigate("/home");
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoader(false);
+      });
+  };
 
   return (
-    <div className='SignUp-body'>
-      <div className='SignUp-inner-container'>
-        <div className='SignUp-right-container'>
-            <h1>Finance App</h1>
-            <h2>The <span style={{color:'#5AFF6A'}}>#1</span> personal finance & budgeting app.</h2>
-            <img src='/assets/loginVector.png'/>
+    <div className="SignUp-body">
+      <div className="SignUp-inner-container">
+        <div className="SignUp-right-container">
+          <h1>Finance App</h1>
+          <h2>The #1 personal finance & budgeting app.</h2>
+          <img src="/assets/loginVector.png" />
         </div>
-        <div className='SignUp-left-container'>
-          <div className='SignUp-left-inner-container'>
+        <div className="SignUp-left-container">
+          <div className="SignUp-left-inner-container">
             <h1>Sign up</h1>
-            <form  onSubmit={(e)=>authenticate(e)}>
-            <input placeholder='Name' type='text' required value={name} onChange={(e)=>setName(e.target.value)} />
-            <input placeholder='Email' type='email' required value={email} onChange={(e)=>setEmail(e.target.value)} />
-            <input placeholder='Password' type='password' required value={password} onChange={(e)=>setPassword(e.target.value)} autoComplete="new-password"/>
-            {!loader && <button type='submit'>Sign up</button>}
-            {!loader && <p>Already have account? <span onClick={()=> navigate('/')} style={{textDecoration:'underline',cursor:'pointer'}}>Sign in</span></p>}
+            <form onSubmit={(e) => authenticate(e)}>
+              <input
+                placeholder="Name"
+                type="text"
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                placeholder="Email"
+                type="email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                placeholder="Password"
+                type="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                autoComplete="new-password"
+              />
+              {!loader && <button type="submit">Sign up</button>}
+              {!loader && (
+                <p>
+                  Already have account?{" "}
+                  <span
+                    onClick={() => navigate("/")}
+                    style={{ textDecoration: "underline", cursor: "pointer" }}
+                  >
+                    Sign in
+                  </span>
+                </p>
+              )}
             </form>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
